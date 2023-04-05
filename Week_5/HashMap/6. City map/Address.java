1fa391a482713ade04ba08946ea51b31636f406c
Rakesh Velavaluri <64448351+VirtualBeetle@users.noreply.github.com>
2023-04-05 14:08:07 +0530

    Create Address.java

---
+++ Week_5/HashMap/6. City map/Address.java
@@ -0,0 +1,74 @@
+import java.util.*;
+
+
+class Address {
+    private String addressLine1;
+    private String addressLine2;
+    private String city;
+    private String state;
+    private Integer pincode;
+    
+    public Address(String addressLine1, String addressLine2, String city, String state, Integer pincode) {
+        this.addressLine1 = addressLine1;
+        this.addressLine2 = addressLine2;
+        this.city = city;
+        this.state = state;
+        this.pincode = pincode;
+    }
+    
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+    
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+    
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+    
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+    
+    public String getCity() {
+        return city;
+    }
+    
+    public void setCity(String city) {
+        this.city = city;
+    }
+    
+    public String getState() {
+        return state;
+    }
+    
+    public void setState(String state) {
+        this.state = state;
+    }
+    
+    public Integer getPincode() {
+        return pincode;
+    }
+    
+    public void setPincode(Integer pincode) {
+        this.pincode = pincode;
+    }
+    
+    public static Map<String, List<Address>> generateCityAddressMap(Map<String, List<Address>> cityAddressMap, Address address) {
+        if (!cityAddressMap.containsKey(address.getCity())) {
+            cityAddressMap.put(address.getCity(), new ArrayList<Address>());
+        }
+        cityAddressMap.get(address.getCity()).add(address);
+        return cityAddressMap;
+    }
+    
+    public static List<Address> searchCity(Map<String, List<Address>> cityAddressMap, String city) {
+        if (cityAddressMap.containsKey(city)) {
+            return cityAddressMap.get(city);
+        } else {
+            return null;
+        }
+    }
+}
