f8f5d50d974969d83e6c0cd3bec3827e985b33ee
Rakesh Velavaluri <64448351+VirtualBeetle@users.noreply.github.com>
2023-04-05 19:11:26 +0530

    Create Address.java

---
+++ Week_4/Concept of Classes and Objects and Encapsulation/1. Customer Address/Address.java
@@ -0,0 +1,13 @@
+class Address {
+	String street;
+    String city;
+    int pincode;
+    String country;
+	
+	void displayAddress() {
+        System.out.println("Street: " + street);
+        System.out.println("City: " + city);
+        System.out.println("Pincode: " + pincode);
+        System.out.println("Country: " + country);
+    }
+}
