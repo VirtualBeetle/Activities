80f1130329099855c338a29feabecb90948aca8c
Rakesh Velavaluri <64448351+VirtualBeetle@users.noreply.github.com>
2023-04-05 14:15:48 +0530

    Create Main.java

---
+++ Week_5/Generics/2. read and display the box values/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the values for length, width and depth: ");
+        int length = scanner.nextInt();
+        int width = scanner.nextInt();
+        int depth = scanner.nextInt();
+
+        Box<Integer, Integer, Integer> box = new Box<>(length, width, depth);
+        box.print();
+    }
+}
