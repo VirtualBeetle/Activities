9cb684b88ae6c042b0fc7b616311a6c53ac5a9cd
Rakesh Velavaluri <64448351+VirtualBeetle@users.noreply.github.com>
2023-04-06 09:39:16 +0530

    Create Hall.java

---
+++ Week_6/1. Hall details/Hall.java
@@ -0,0 +1,39 @@
+public class Hall {
+    private String name;
+    private Double costPerDay;
+    private String owner;
+
+    public Hall() {
+        // Default constructor
+    }
+
+    public Hall(String name, Double costPerDay, String owner) {
+        this.name = name;
+        this.costPerDay = costPerDay;
+        this.owner = owner;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getCostPerDay() {
+        return costPerDay;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCostPerDay(Double costPerDay) {
+        this.costPerDay = costPerDay;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+}
