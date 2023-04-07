c8bbd7400a65aa6fc0de8f1e3d55fa1f17e6cd6c
Rakesh Velavaluri <64448351+VirtualBeetle@users.noreply.github.com>
2023-04-08 00:18:35 +0530

    Create Role.java

---
+++ Week_5/Comparator and Comparable Interfaces/3. Lanchester bank/Role.java
@@ -0,0 +1,21 @@
+public class Role {
+	private String name;
+
+	public Role() {
+		super();
+	}
+
+	public Role(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
