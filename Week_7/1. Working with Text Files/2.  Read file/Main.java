3ef73c29d334d9ae69b27a88130309e9f45f659b
Rakesh Velavaluri <64448351+VirtualBeetle@users.noreply.github.com>
2023-04-11 19:53:06 +0530

    Create Main.java

---
+++ Week_7/1. Working with Text Files/2.  Read file/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        StallBO stallBO = new StallBO();
+        Stall stall = stallBO.readStallFromFile();
+        if (stall != null) {
+            stallBO.display(stall);
+        }
+    }
+}
